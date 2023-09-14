fdcb2106bfc307f93e04771eb09ca833aacb4d37
zayed <zaidgalgali69@gmail.com>
2023-09-14 12:24:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
-import BannerSlider from './components/BannerSlider';
-import Section from './components/Section';
-import ThemeSwitch from './components/ThemeSwitch';
-import Card from './components/Card'; // Import your Card component here
-import { bannerData, sectionData, cardData } from './Data';
+import React, { useState } from "react";
+import "./App.css";
+import BannerSlider from "./components/BannerSlider";
+import Section from "./components/Section";
+import ThemeSwitch from "./components/ThemeSwitch";
+import Card from "./components/Card"; // Import your Card component here
+import { bannerData, sectionData, cardData } from "./Data";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -14,11 +14,16 @@ function App() {
   };
 
   return (
-    <div className={`App ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+    <div className={`App ${isDarkTheme ? "dark-theme" : "light-theme"}`}>
       <ThemeSwitch isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
       <BannerSlider data={bannerData} isDarkTheme={isDarkTheme} />
       {sectionData.map((section) => (
-        <Section key={section.id} data={section} cards={cardData} isDarkTheme={isDarkTheme} />
+        <Section
+          key={section.id}
+          data={section}
+          cards={cardData}
+          isDarkTheme={isDarkTheme}
+        />
       ))}
       {/* {cardData.map((card) => (
         <Card key={card.id} data={card} isDarkTheme={isDarkTheme} />
